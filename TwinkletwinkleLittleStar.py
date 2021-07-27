43a2bbbe015762a5f174aa89fd816de4bfce4b01
Arnab-Mukhopadhyay-hash <84343247+Arnab-Mukhopadhyay-hash@users.noreply.github.com>
2021-07-27 22:38:09 +0530

    Add files via upload

---
+++ TwinkletwinkleLittleStar.py
@@ -0,0 +1,4 @@
+
+# Online IDE - Code Editor, Compiler, Interpreter
+
+print("Twinkle, twinkle, little star, \n \t How I wonder what you are! \n\t\t Up above the sky so high, \n\t\tLike a diamond in the sky.\nTwinkle, twinkle, little star, \n\t How I wonder what you are.")
